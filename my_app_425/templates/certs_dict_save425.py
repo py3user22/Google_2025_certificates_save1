da349bb85ae679cd6fc3ae5f7e7c2a0b9b604529
Jesus G <113757515+py3user22@users.noreply.github.com>
2025-04-25 18:31:16 -0500

    1st commit

---
+++ my_app_425/templates/certs_dict_save425.py
@@ -0,0 +1,135 @@
+"""042525' certs list save"""
+
+
+# 2025' certs
+Networking_in_Google_Cloud = {
+    1: 'Networking in Google Cloud: Fundamentals',
+    1.1: 'complete: 05-2023',
+    2: 'Networking in Google Cloud: Routing & Addressing',
+    2.1: 'complete: 07-2023',
+    3: 'Networking in Google Cloud: Network Architecture',
+    3.1: 'complete: 03-2025',
+    4: 'Networking in Google Cloud: Network Security',
+    4.1: 'complete: 03-2025',
+    5: 'Networking in Google Cloud: Load Balancing',
+    5.1: 'complete: 04-2025',
+    6: 'Networking in Google Cloud: Hybrid & Multicloud',
+    6.1: 'complete: in progress',
+}
+
+Preparing_Professional_Cloud_Architect_Journey = {
+    1: 'Google Cloud: Fundamentals: Core Infrastructure',
+    1.1: 'complete: 06-2023',
+    2: 'Essential Google Cloud: Foundation',
+    2.1: 'complete: 02-2025',
+    3: 'Essential Google Cloud Infrastructure: Core Services',
+    3.1: 'complete: 02-2025',
+    4: 'Elastic Google Cloud: Scaling & Automation',
+    4.1: 'complete: 03-2025',
+    5: 'Reliable Google Cloud Infrastructure: Design & Process',
+    5.1: 'complete: 04-2025',
+    6: 'Architecting with Google Kubernetes Engine: Foundations',
+    6.1: 'complete: 06-2023',
+    7: 'Preparing for your Professional Cloud Architect Journey.',
+    7.1: 'complete: 04-2025',
+}
+
+
+"""
+--- https://www.cloudskillsboost.google/
+--- Skill badge
+--- Set Up an App Dev Environment on Google Cloud
+--- Status: Complete --04-2025
+"""
+Labs_required = [
+    'Cloud Storage: Qwik Start - Cloud Console',
+    'Cloud Storage: Qwik Start - CLI/SDK',
+    'Cloud IAM: Qwik Start',
+    'Cloud Monitoring: Qwik Start',
+    'Cloud Run Functions: Qwik Start -Console',
+    'Cloud Run Functions: Qwik Start -CLI',
+    'Pub/Sub: Qwik Start -Console',
+    'Pub/Sub: Qwik Start -CLI',
+    'Pub/Sub: Qwik Start -Python',
+    'Set up App Dev Environment on Google Cloud: Challenge lab',
+]
+
+
+"""
+--- https://www.cloudskillsboost.google/
+--- Skill badge
+--- Preparing for Cloud Network Engineer Journey 
+--- Status: in-progress  --21%
+--- Total time: 24 hours
+--- Designation: Advanced
+"""
+
+Cloud_Network_Engineer_Journey = {
+    1: 'Designing & planning a Google Cloud Network',
+    2: 'Implementing VPC -Virtual private cloud networks',
+    3: 'Configuring managed network services',
+    4: 'Implementing hybrid network interconnectivity',
+    5: 'Managing, monitoring, troubleshooting network operations',
+}
+
+
+"""
+--- https://www.cloudskillsboost.google/
+--- Skill badge
+--- Preparing for Cloud Security Engineer Journey 
+--- Status: in-progress  --23%
+--- Total time: 8 hours
+--- Designation: Introductory
+"""
+
+Cloud_Security_Engineer_Journey = {
+    1: 'Configuring Access w/in a Cloud Solution environment',
+    2: 'Configuring Network Security',
+    3: 'Ensuring data protection',
+    4: 'Managing operations in a cloud environment',
+    5: 'Ensuring compliance',
+}
+
+
+"""
+--- https://www.cloudskillsboost.google/
+--- Skill badge
+--- Networking in Google Cloud: Fundamentals path
+--- Status: in-progress  --29%
+--- Total time: 5 hours
+--- Designation: Intermediate
+"""
+
+Networking_in_Google_Cloud_fundamentals = {
+    1: 'Welcome to Networking in Google Cloud',
+    2: 'VPC -virtual private cloud networking fundamentals',
+    3: 'Sharing VPC networks',
+    4: 'Network Monitoring & Logging',
+}
+
+"""
+--- https://www.cloudskillsboost.google/
+--- Skill badge
+--- DevSecOps path 
+--- Status: 12%
+--- Total time: 93.25 hours
+--- Designation: Intermediate
+"""
+
+courses = {
+    1: 'Google Cloud Fundamentals: Core Infrastructure',
+    2: 'Architecting with Google Kubernetes Engine: Foundations',
+    3: 'Architecting with Google Kubernetes Engine: Workloads',
+    4: 'Architecting with Google Kubernetes Engine: Production',
+    5: 'Managing Security in Google Cloud',
+    6: 'Security best practices in Google Cloud',
+    7: 'Mitigating Security Vulnerabilities on Google Cloud',
+    8: 'Getting started w/ Terraform for Google Cloud',
+    9: 'Security & Identity Fundamentals',
+    10: 'Implement CI/CD pipelines on Google Cloud',
+    11: 'DevOps Essentials',
+    12: 'Google Kubernetes Engine Best Practices: Security',
+
+
+
+}
