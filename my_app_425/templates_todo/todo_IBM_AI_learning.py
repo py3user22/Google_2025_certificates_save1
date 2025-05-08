9b74c2cb72b5ee03c97e40076dd55fd05fbfd4fe
Jesus G <113757515+py3user22@users.noreply.github.com>
2025-05-08 04:45:48 -0500

    508 save1

---
+++ my_app_425/templates_todo/todo_IBM_AI_learning.py
@@ -0,0 +1,60 @@
+"""508 IBM AI training """
+
+
+IBM_AI_Engineering_Professional_Certificate = {
+    0: 'https://www.coursera.org/programs/m-uq19a/professional-certificates/ai-engineer?',
+    0.1: '------------------ ',
+    1: '20hr -- Machine learning w/ Python',
+    2: '9hr -- Introduction to Deep learning & neural networks',
+    3: '23hr -- Deep Learning w/ Keras & Tensorflow',
+    4: '17hr -- Intro to Neural Networks & PyTorch',
+    5: '20hr -- Deep Learning w/ PyTorch',
+    6: '16hr -- AI Capstone project w/ deep learning',
+    7: '5hr -- Generative AI & LLMs, Architecture & date preparation',
+    8: '9hr -- Generative AI Foundational Models for NLP & language ',
+    9: '8hr -- Generative AI Language modeling w/ Transformers',
+    10: '10hr -- Generative AI Engineering & fine-tuning transformers',
+    11: '11hr -- Generative AI advance fine-tuning for LLMs',
+    12: '12hr -- Fundamentals of AI agents using RAG & LangChain',
+    13: '13hr -- Project: Generative AI applications w/ RAG & language',
+
+}
+
+
+IBM_AI_Developer_Professional_Certificate = {
+    0: 'https://www.coursera.org/programs/m-uq19a/professional-certificates/applied-artifical-intelligence-ibm-watson-ai?',
+    0.1: '------------------ ',
+    1: '14hr -- Intro to Software engineering',
+    2: '12hr -- Intro to AI Artificial Intelligence',
+    3: '7hr -- Generative AI, Introduction & applications',
+    4: '7hr -- Generative AI, Prompt engineering basics',
+    5: '13hr -- Intro to HTML, CSS, JavaScript',
+    6: '25hr -- Python for Data Science, AI, & development',
+    7: '11hr -- Developing AI application w/ Python & Flask',
+    8: '13hr -- Building generative AI-powered applications w/ Python',
+    9: '17hr -- Generative AI, elevate your software dev. capstone',
+    10: '10hr -- Software dev. career guide & interview preparation',
+}
+
+
+IBM_Generative_AI_Engineering_Professional_Certificate = {
+    0: 'https://www.coursera.org/programs/m-uq19a/professional-certificates/ibm-generative-ai-engineering?',
+    0.1: '------------------ ',
+    1: '12hr -- Intro to AI Artificial Intelligence',
+    2: '7hr -- Generative AI, Introduction & applications',
+    3: '7hr -- Generative AI, Prompt engineering basics',
+    4: '25hr -- Python for Data Science, AI, & development',
+    5: '11hr -- Developing AI application w/ Python & Flask',
+    6: '13hr -- Building generative AI-powered applications w/ Python',
+    7: '15hr -- Data Analysis w/ Python',
+    8: '20hr -- Machine learning w/ Python',
+    9: '9hr -- Intro to deep learning & neural networks',
+    10: '5hr -- Generative AI & LLMs, Architecture & date preparation',
+    11: '9hr -- Generative AI Foundational Models for NLP & language ',
+    12: '8hr -- Generative AI Language modeling w/ Transformers',
+    13: '8hr -- Generative AI Engineering & fine-tuning transformers',
+    14: '9hr -- Generative AI Advance fine-tuning for LLMs',
+    15: '6hr -- Fundamentals of AI agents using RAG & LangChain',
+    16: '9hr -- Generative AI apps w/ RAG & language',
+}
+
